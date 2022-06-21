35757def925c7ff1c2391d9f5dc9e25e5e2c25d7
Ilya Amursky <ilya@amursky.me>
2022-06-21 20:03:52 +0200

    fix: use env variable for redirects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,10 @@ import type { AppProps } from "next/app";
 
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,56 @@
 import type { NextPage } from "next";
 
-import { Fragment } from "react";
-import { LoginButton } from "../components/login-button";
+import { CognitoAuth } from "amazon-cognito-auth-js";
+import { Fragment, useCallback, useState } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const { data } = useSession();
+
+  const [redirecting, setRedirecting] = useState<boolean>(false);
+
+  const handleClickSignIn = useCallback(() => {
+    setRedirecting(true);
+    signIn("cognito");
+  }, []);
+
+  const handleClickSignOut = useCallback(() => {
+    setRedirecting(true);
+
+    signOut().then(() => {
+      const cognitoAuth = new CognitoAuth({
+        AppWebDomain: `${process.env.NEXT_PUBLIC_COGNITO_APP_WEB_DOMAIN}`,
+        ClientId: `${process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID}`,
+        RedirectUriSignIn: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
+        RedirectUriSignOut: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
+        TokenScopesArray: ["email"],
+      });
+
+      cognitoAuth.signOut();
+    });
+  }, []);
+
   return (
     <Fragment>
       <Head>
         <title>Cognito + NextAuth.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <LoginButton />
+
+      {data ? (
+        <button onClick={handleClickSignOut} disabled={redirecting}>
+          Sign out
+        </button>
+      ) : (
+        <button onClick={handleClickSignIn} disabled={redirecting}>
+          Sign in
+        </button>
+      )}
+
+      <pre>
+        <code>{data ? JSON.stringify(data, null, 2) : ""}</code>
+      </pre>
     </Fragment>
   );
 };
